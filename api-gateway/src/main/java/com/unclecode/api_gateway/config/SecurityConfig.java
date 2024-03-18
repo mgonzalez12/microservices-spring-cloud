26b75fd8cf616c5684028dc0b349cb4b431fafbb
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2024-03-18 20:03:22 +0100

    fix(MICROSERVICES): KeyCloak - resilience
    Seguridad y manerjo de errores

---
+++ api-gateway/src/main/java/com/unclecode/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.unclecode.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorize -> authorize.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults()); // Use Customizer with defaults for OAuth 2.0 login
+
+        return http.build();
+    }
+}
+
---
+++ products-service/src/main/java/com/unclecode/products_service/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.unclecode.products_service.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Use the new syntax for disabling CSRF
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()) // Updated authorization configuration
+                // .and() is no longer needed for chaining
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter()))); // JWT configuration
+
+        return http.build();
+    }
+
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+
+        return converter;
+    }
+}
+
+@SuppressWarnings("unchecked")
+class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>>{
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        if (jwt.getClaims() == null){
+            return List.of();
+        }
+
+        final Map<String, List<String>> realmAccess = (Map<String, List<String>>) jwt.getClaims().get("realm_access");
+
+        return realmAccess.get("roles").stream()
+                .map(roleName -> "ROLE_" + roleName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
